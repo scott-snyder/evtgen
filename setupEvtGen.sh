3855be75659961eaf884e54d833b778a16b58f36
Thomas Latham <T.Latham@warwick.ac.uk>
2023-08-21 16:56:40 +0100

    Update version number to 2.2.1 in CMakeLists.txt and setupEvtGen.sh

--- setupEvtGen.sh
+++ setupEvtGen.sh
@@ -42,7 +42,7 @@
 INSTALL_PREFIX="/usr/local"
 
 # EvtGen version or tag number (or branch name). No extra spaces on this line!
-VERSION=R02-02-00
+VERSION=R02-02-01
 
 # HepMC version numbers - change HEPMCMAJORVERSION to 2 in order to use HepMC2
 HEPMCMAJORVERSION="3"
